18f19cdf3f50b6613ffdf1e14a633c4c13fa187e
kaden-kykim <kaden.kykim@gmail.com>
2020-03-31 04:02:19 -0700

    Change enum to NS_ENUM

--- Lab8/PizzaRestaurant/Pizza.h
+++ Lab8/PizzaRestaurant/Pizza.h
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum : NSUInteger {
+typedef NS_ENUM(NSUInteger, PizzaSize) {
     SMALL,
     MEDIUM,
     LARGE,
     UNDEFINED = NSUIntegerMax
-} PizzaSize;
+};
 
 @interface Pizza : NSObject
 
