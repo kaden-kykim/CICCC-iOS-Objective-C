53f70d591f6fa470f9a27c33ac4c001dc21c4343
kaden-kykim <kaden.kykim@gmail.com>
2020-04-07 14:27:12 -0700

    Create Lab6: Threelow

---
+++ Lab6/Threelow/Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Kaden Kim on 2020-04-07.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
