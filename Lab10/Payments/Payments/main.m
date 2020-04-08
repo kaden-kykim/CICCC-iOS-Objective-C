2e84be8c250fd172aa94c426aca0af334b0dd622
kaden-kykim <kaden.kykim@gmail.com>
2020-04-08 13:24:23 -0700

    Add Lab10: Payments project

---
+++ Lab10/Payments/Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Kaden Kim on 2020-04-08.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
