686771f2532ed1a003b1cdfd24429ec64647a6bc
kaden-kykim <kaden.kykim@gmail.com>
2020-04-08 13:37:56 -0700

    [Lab10] Add 'Setup Output and Input in main.m' section

--- Lab10/Payments/Payments/main.m
+++ Lab10/Payments/Payments/main.m
@@ -8,10 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+NSString *userInput() {
+    char buf[256];
+    return [[NSString stringWithUTF8String:fgets(buf, 256, stdin)] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSInteger total = arc4random_uniform(901) + 100;
+        NSLog(@"Thank you for shopping at Acme.com.");
+        NSLog(@"Your total is $%ld", total);
+        NSLog(@"Please select your payment method: 1. Paypal, 2. Stripe, 3. Amazon");
+        int inputNum = [[[NSNumberFormatter new] numberFromString:userInput()] intValue];
+        if (inputNum > 0 && inputNum <= 3)
+            NSLog(@"You selected %d", inputNum);
+        else
+            NSLog(@"Please input again.");
     }
     return 0;
 }
