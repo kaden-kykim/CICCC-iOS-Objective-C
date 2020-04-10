65231b3a7394ab8e1ebb6d08d92eb3c6942a8267
kaden-kykim <kaden.kykim@gmail.com>
2020-04-10 13:35:44 -0700

    Add clusters

--- Lab13/Lab13/PigLatin/NSString+PigLatin.m
+++ Lab13/Lab13/PigLatin/NSString+PigLatin.m
@@ -11,11 +11,12 @@
 @implementation NSString (PigLatin)
 
 - (NSString *)stringByPigLatinization {
+    NSArray *clusters = @[@"ch", @"sh", @"sm", @"st", @"th", @"ps", @"ph", @"pl", @"gl"];
     NSCharacterSet *vowels = [NSCharacterSet characterSetWithCharactersInString:@"aeiou"];
     NSMutableArray *words = [[[self lowercaseString] componentsSeparatedByString:@" "] mutableCopy];
     for (int i = 0; i < [words count]; ++i) {
         NSArray *tokens = [words[i] componentsSeparatedByCharactersInSet:vowels];
-        words[i] = ([vowels characterIsMember:[words[i] characterAtIndex:0]] || [tokens count] == 1) ? [[words[i] stringByAppendingString:@"ay"] capitalizedString] : [[[[words[i] substringFromIndex:[tokens[0] length]] stringByAppendingString:tokens[0]] stringByAppendingString:@"ay"] capitalizedString];
+        words[i] = ([clusters containsObject:[[words[i] substringToIndex:2] lowercaseString]]) ? words[i] = [[[words[i] substringFromIndex:2] stringByAppendingFormat:@"%@ay", [words[i] substringToIndex:2]] capitalizedString] : ([vowels characterIsMember:[words[i] characterAtIndex:0]] || [tokens count] == 1) ? [[words[i] stringByAppendingString:@"ay"] capitalizedString] : [[[words[i] substringFromIndex:[tokens[0] length]] stringByAppendingFormat:@"%@ay", tokens[0]] capitalizedString];
     }
     return [words componentsJoinedByString:@" "];
 }
