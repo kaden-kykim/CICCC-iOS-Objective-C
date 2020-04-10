2e88d2ee16b59f1281afcffbc41fc19b88a8fc64
kaden-kykim <kaden.kykim@gmail.com>
2020-04-10 13:47:12 -0700

    [Lab13] Delete unnecessary term

--- Lab13/Lab13/PigLatin/NSString+PigLatin.m
+++ Lab13/Lab13/PigLatin/NSString+PigLatin.m
@@ -16,7 +16,7 @@
     NSMutableArray *words = [[[self lowercaseString] componentsSeparatedByString:@" "] mutableCopy];
     for (int i = 0; i < [words count]; ++i) {
         NSArray *tokens = [words[i] componentsSeparatedByCharactersInSet:vowels];
-        words[i] = ([clusters containsObject:[[words[i] substringToIndex:2] lowercaseString]]) ? words[i] = [[[words[i] substringFromIndex:2] stringByAppendingFormat:@"%@ay", [words[i] substringToIndex:2]] capitalizedString] : ([vowels characterIsMember:[words[i] characterAtIndex:0]] || [tokens count] == 1) ? [[words[i] stringByAppendingString:@"ay"] capitalizedString] : [[[words[i] substringFromIndex:[tokens[0] length]] stringByAppendingFormat:@"%@ay", tokens[0]] capitalizedString];
+        words[i] = ([clusters containsObject:[[words[i] substringToIndex:2] lowercaseString]]) ? [[[words[i] substringFromIndex:2] stringByAppendingFormat:@"%@ay", [words[i] substringToIndex:2]] capitalizedString] : ([vowels characterIsMember:[words[i] characterAtIndex:0]] || [tokens count] == 1) ? [[words[i] stringByAppendingString:@"ay"] capitalizedString] : [[[words[i] substringFromIndex:[tokens[0] length]] stringByAppendingFormat:@"%@ay", tokens[0]] capitalizedString];
     }
     return [words componentsJoinedByString:@" "];
 }
